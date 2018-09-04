6edd076552161fa0649aacb9007d83044914b7f0
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-09-04 14:18:11 +0200

    Revert "Allow launcher to be hosted in a url path"
    This reverts commit da4e4cc73c830e9449e627d531753bcd4c95661a.

--- ewatercycle_experiment_launcher/serve.py
+++ ewatercycle_experiment_launcher/serve.py
@@ -9,8 +9,6 @@ logging.basicConfig(level=logging.INFO)
 app = connexion.FlaskApp(__name__)
 app.app.config['JUPYTERHUB_TOKEN'] = os.environ['JUPYTERHUB_TOKEN']
 app.app.config['JUPYTERHUB_URL'] = os.environ['JUPYTERHUB_URL']
-base_path = os.environ.get('LAUNCHER_PREFIX', '/')
-arguments = {'basepath': base_path}
 resolver = RestyResolver('ewatercycle_experiment_launcher.api')
-app.add_api('swagger.yaml', resolver=resolver, arguments=arguments, base_path=base_path)
+app.add_api('swagger.yaml', resolver=resolver)
 CORS(app.app)
