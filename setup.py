f77724dc167cf8495a1a8dfb62ac6955bc64edd6
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-12-06 11:50:30 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ewatercycle/ewatercycle_experiment_launcher',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     package_data={
       'ewatercycle_experiment_launcher': ['swagger.yaml'],
